f1e635aea05fb35d10bd1b5a8620dc96d19a8423
Taylor Engel <tengel92@gmail.com>
2022-10-26 20:21:31 -0500

    fix iOS notch styling

--- src/components/router-head/router-head.tsx
+++ src/components/router-head/router-head.tsx
@@ -13,7 +13,11 @@ export const RouterHead = component$(() => {
       <title>{head.title}</title>
 
       <link rel="canonical" href={loc.href} />
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1.0, viewport-fit=cover"
+      />
+      <meta name="theme-color" content="#312e81"></meta>
       <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
 
       <link rel="preconnect" href="https://fonts.googleapis.com" />
